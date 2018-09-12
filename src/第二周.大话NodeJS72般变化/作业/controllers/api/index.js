98e84a964d4e8e18509351cdf618f486bb3e5bdc
JackFGreen <clcortana@163.com>
2018-09-12 15:00:14 +0800

    add php sql

--- src/第二周.大话NodeJS72般变化/作业/controllers/api/index.js
+++ src/第二周.大话NodeJS72般变化/作业/controllers/api/index.js
@@ -1,15 +1,24 @@
 const api = require('koa-router')()
+const axios = require('axios')
 
 api
   .post('/praise', postPraise)
 
 async function postPraise(ctx, next) {
-  ctx.body = {
-    code: 200,
-    data: {
-      count: ctx.request.body.count
-    },
-    message: ''
+  const res = await axios.get('http://192.168.64.2/phptest/koa_test/?count=' + ctx.request.body.count)
+  console.log(res.data)
+  if (res.data.code === 200) {
+    const count = res.data.count
+    ctx.body = {
+      code: 200,
+      data: {
+        count
+      },
+      message: ''
+    }
+  } else {
+    ctx.status = 500
+    ctx.body = 'error...'
   }
 }
 
