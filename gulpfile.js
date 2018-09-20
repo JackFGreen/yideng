dd7ab5365396530612ab3438ddf811b9ee968cbe
JackFGreen <clcortana@163.com>
2018-09-20 09:39:42 +0800

    add layout file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp')
 const connect = require('gulp-connect')
+const ip = require('ip')
+
+const host = ip.address()
 
 gulp.task('webserver', () => {
   connect.server({
     livereload: true,
+    host,
     port: 9527
   })
 })
